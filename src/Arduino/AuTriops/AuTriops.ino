001272fbff5b5576140730d66255c5818b43354b
RoDoerIng <ronnydoering.ing@gmail.com>
2017-10-18 15:30:25 +0200

    sprint2

--- src/Arduino/AuTriops/AuTriops.ino
+++ src/Arduino/AuTriops/AuTriops.ino
@@ -2,16 +2,25 @@
 
 #define ONE_WIRE_BUS 6
 #define BUZZER 5
+#define QUIT_BUTTON 2
+#define HIGH_TEMP 27.0
+#define LOW_TEMP 20.0
 #define DEBUG false
 
 OneWire oneWire(ONE_WIRE_BUS);
 DallasTemperature sensors(&oneWire);
 DeviceAddress tsaddress;
 
+float temp;
+bool globalQuit;
+
+
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
   pinMode(BUZZER, OUTPUT);
-  Serial.begin(9600);
+  pinMode(QUIT_BUTTON, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(QUIT_BUTTON), setQuit, FALLING);
+  Serial.begin(115200);
   sensors.begin();
   debug("" + sensors.getDeviceCount());
   debugln(" Geraete am Bus.");
@@ -19,45 +28,97 @@ void setup() {
 }
 
 void loop() {
-  float temp = getTemperature();
+  temp = getTemperature();
   
   Serial.println("act temp: "+floatToString(temp));
   
+  alarmController();
   
-  
-  if (temp > 27.0){
-    Serial.println("Temperature too HIGH!");
-    setAlarm();
-  }
-  
-  else if (temp < 20.0){
-    debugln("Temperature too LOW!");
+  ackMonitor();
+
+  //temperatureMonitor();
+  //Serial.println("temp mon");
+
+  delay(1000);
+}
+
+
+void alarmController(){
+  if (!isTempOk() && !isQuit()){
     setAlarm();
   }
   else {
     debugln("no Alarm");
     resetAlarm();
   }
-  
-  
-  delay(1000);
 }
-
 
 // set Alarm
 void setAlarm(){
   digitalWrite(LED_BUILTIN, true);
-  digitalWrite(BUZZER, true);
+  tone(BUZZER, 3);
 }
 
 void resetAlarm(){
   digitalWrite(LED_BUILTIN, false);
-  digitalWrite(BUZZER, false);
+  noTone(BUZZER);
 }
 
+// Button
 
+bool isAckButtonPressed(){
+  return !digitalRead(QUIT_BUTTON);
+}
+
+// Acknowledge
+
+void ackMonitor(){
+  if (isAckButtonPressed()){
+    setQuit();
+  }
+
+  if (isTempOk()){
+    resetQuit();
+  }
+
+  if (isQuit()){
+    debugln("quittiert!");
+  }
+}
+
+void setQuit(){
+  globalQuit = true;
+}
+
+void resetQuit(){
+  globalQuit = false;
+}
+
+bool isQuit(){
+  return globalQuit;
+}
 
 // Temperature
+
+void temperatureMonitor(){
+  if (!isTempOk()){
+    Serial.println("Temperature out of Range! " + floatToString(temp));
+  }
+  else{
+    Serial.print("Temperature OK! ");
+    Serial.println(floatToString(temp));
+  }
+}
+
+bool isTempOk(){
+  if (temp <= HIGH_TEMP && temp >= LOW_TEMP){
+    debugln("Temp OK");
+    return true;
+  }
+  else{
+    return false;
+  }
+}
 
 float getTemperature(){
   float temp;
